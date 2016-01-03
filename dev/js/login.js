479b43674052fb1b024c73f89e573530e7e4b36c
wenwu@7_PC <galaxywuww@gmail.com>
2016-01-04 00:53:44 +0800

    migrate to ng framework

---
+++ dev/js/login.js
@@ -0,0 +1,42 @@
+require.config({
+    baseUrl: './lib/',
+    paths: {
+        'jquery': 'jquery/dist/jquery',
+        'constant': '../js/common/constant'
+    },
+    shim: {}
+});
+
+require(['constant'],function(CONSTANT){
+    document.forms[0].onsubmit = function(){
+        return login();
+    }
+
+    function login() {
+        var userName = document.getElementById('username').value;
+        var passWord = document.getElementById('password').value;
+
+        var xmlhttp;
+        if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else { // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = eval('(' + xmlhttp.responseText + ')');
+                console.log(response);
+                if(response.code === 0){
+                    sessionStorage.setItem('userInfo', JSON.stringify(response.data));
+                    location.href = '/index.html';
+                }else{
+                    alert(response.msg);
+                }
+            }
+        }
+        xmlhttp.open('POST', CONSTANT.SERVER_HOST + '/user/login?username=' + userName + '&password=' + passWord, true);
+        xmlhttp.send();
+
+        return false;
+    }
+});
